c33f8595b726eef369699146127c4843e3f7220a
Russ Frank <russ.frank@rutgers.edu>
2012-10-28 12:46:40 -0400

    PlacesServer: had to explicitly set policy

--- places_server/PlacesServer.java
+++ places_server/PlacesServer.java
@@ -4,24 +4,25 @@ import java.rmi.server.*;
  * Server for the Places search. This just sets up the RMI server.
  */
 public class PlacesServer{
-	public static void main(String args[]) {
+  public static void main(String args[]) {
+    System.setProperty("java.security.policy","policy");
         //TODO: make port optional.
-		if (args.length != 1) {
-			System.err.println("usage: java PlacesServer rmi_port");
-			System.exit(1);
-		}
-		// Create and install a security manager
-		if (System.getSecurityManager() == null)
-			System.setSecurityManager(new RMISecurityManager());
-		try {
-			int port = Integer.parseInt(args[0]);
-			String url = "//localhost:" + port + "/PlacesSearch";
-			Naming.rebind(url, new PlacesSearch());
-			System.out.println("server " + url + " is running...");
-		}
-		catch (Exception e) {
-			System.err.println("PlacesSearch server failed:" + e.getMessage());
+    if (args.length != 1) {
+      System.err.println("usage: java PlacesServer rmi_port");
+      System.exit(1);
+    }
+    // Create and install a security manager
+    if (System.getSecurityManager() == null)
+      System.setSecurityManager(new RMISecurityManager());
+    try {
+      int port = Integer.parseInt(args[0]);
+      String url = "//localhost:" + port + "/PlacesSearch";
+      Naming.rebind(url, new PlacesSearch());
+      System.out.println("server " + url + " is running...");
+    }
+    catch (Exception e) {
+      System.err.println("PlacesSearch server failed:" + e.getMessage());
             e.printStackTrace();
-		}
-	}
+    }
+  }
 }
