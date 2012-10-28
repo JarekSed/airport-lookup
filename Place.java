b4f37473f7774e52f6465fdfea0abdc6b471ce95
Russ Frank <russ.frank@rutgers.edu>
2012-10-28 12:50:13 -0400

    Everything -> 1 dir to avoid classpath fuckery

---
+++ Place.java
@@ -0,0 +1,24 @@
+import java.io.Serializable;
+
+/*
+ * Simple class to store data about a place so it can be marshalled for RMI calls.
+ */
+
+public class Place implements Serializable{
+    public Place(String name, String state, double latitude, double longitude){
+        this.name = name;
+        this.state = state;
+        this.latitude = latitude;
+        this.longitude=longitude;
+    }
+
+    public String getName() { return name; }
+    public String getState() { return state; }
+    public double getLatitude() { return latitude; }
+    public double getLongitude() { return longitude; }
+    private String name;
+    private String state;
+    private double latitude;
+    private double longitude;
+
+}
