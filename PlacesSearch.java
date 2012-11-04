c3993a28c7b1911fedb00ef7343d781787fbb572
Jarek Sedlacek <jarek@cs.rutgers.edu>
2012-11-04 17:08:52 -0500

    Fixed a bug in PlacesSearch. Instead of backing our search with a TreeMap, it is now backed with a HashMap of TreeMaps, where each state has its own TreeMap of places in the state.

--- PlacesSearch.java
+++ PlacesSearch.java
@@ -3,6 +3,7 @@ import java.rmi.server.*;
 import java.util.HashMap;
 import java.io.*;
 import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.TreeMap;
 import java.util.Comparator;
 
@@ -12,27 +13,22 @@ import java.util.Comparator;
  */
 public class PlacesSearch extends UnicastRemoteObject implements PlacesInterface{
 
+
+    /* Comparator for the TreeMap of places in each state.
+     * Compares places based on their names, but only using the first k chars,
+     * where k is the size of the shorted string. This is so we can search for things like 'new york' to find 'new york city'
+     */
     private class PlacesCompare implements Comparator<String> {
         public int compare(String first, String second){
-            // Last 3 chars of each are , and the state code
-            String first_name = first.substring(0,first.length() -3);
-            String first_state = first.substring(first.length() -2);
-            String second_name = second.substring(0,second.length() -3);
-            String second_state = second.substring(second.length() -2);
-
-            int min_length = Math.min(first_name.length(), second_name.length());
-            int res = first_name.substring(0,min_length-1).toLowerCase().compareTo(second_name.substring(0,min_length-1).toLowerCase());
-            if (res != 0) {
-                return res;
-            } else {
-                return first_state.toLowerCase().compareTo(second_state.toLowerCase()); 
-            }
+            int min_length = Math.min(first.length(), second.length());
+            return first.substring(0,min_length).toLowerCase().compareTo(second.substring(0,min_length).toLowerCase());
         }
     }
 
 
     public PlacesSearch() throws RemoteException {
         try{
+            // Build our lookup table from the places data file.
             lookup_table = parse_places_file("places2k.txt");
         } catch (IOException e){
             System.err.println(e.getMessage());
@@ -44,32 +40,52 @@ public class PlacesSearch extends UnicastRemoteObject implements PlacesInterface
     /* Implementation of PlacesInterface.find_place
      */
     public Place find_place(String city, String state) throws RemoteException {
-        Place place = lookup_table.get(city.trim() + "," + state.trim());
-        return place;
+        // Look up the TreeMap of all places in this state.
+        TreeMap<String,Place> places_in_state = lookup_table.get(state.trim().toLowerCase());
+        // If we have places in this state, look for the place that matches the name we got.
+        // This will return null if we can't find any place that matches the name.
+        if (places_in_state != null) {
+            return places_in_state.get(city);
+        // otherwise, return null.
+        } else {
+            return null;
+        }
     }
 
     /**
      * Reads census data file, and builds a lookup table containing Place objects. 
      * @throws IOException if file cannot be accessed.
      * @param  file_path path of the file to read.
-     * @return Hashmap of "city,state" strigns to Place objects.
+     * @return Hashmap of state to TreeMaps of name to place objects. So, each state has a TreeMap of all places in that state, and the TreeMaps
+     *  are sorted using the name of the place as keys. They are compared using the PlacesCompare Comparator.
      */
-    private TreeMap<String,Place> parse_places_file(String file_path) throws IOException,ParseException{
-        TreeMap<String,Place> places = new TreeMap<String,Place>(new PlacesCompare()); 
+    private HashMap<String,TreeMap<String,Place>> parse_places_file(String file_path) throws IOException,ParseException{
+        HashMap<String,TreeMap<String,Place>> places = new HashMap<String, TreeMap<String,Place>>();
         BufferedReader br = new BufferedReader(new InputStreamReader(new DataInputStream(new FileInputStream(file_path)))); 
         String line;
-        // We keep track of what line we are on so we can report errors better.
-        int line_number =1;
         while((line = br.readLine()) != null) {
+            // Ignore lines that are only whitespace.
+            if (line.trim().length() == 0) {
+                continue;
+            }
+            // Pull out data from each field
             String state = line.substring(0,2);
             String name = line.substring(9,73).trim();
             double latitude = Double.parseDouble(line.substring(143,153).trim());
             double longitude = Double.parseDouble(line.substring(153,163).trim());
+
             Place place = new Place(name,state,latitude, longitude);
-            places.put(name.trim() + "," + state.trim(), place);
-            line_number++;
+            // Initialize TreeMap for this state if it hasn't been initialized yet
+            if (! places.containsKey(state.trim().toLowerCase())) {
+                places.put(state.trim().toLowerCase(), new TreeMap<String,Place>(new PlacesCompare()));
+            }
+            // Insert our place object into the TreeMap for this state.
+            places.get(state.trim().toLowerCase()).put(name,place);
         }
         return places;
     }
-    private TreeMap<String,Place> lookup_table;
+    // Used for quick lookups of places. This is a HashMap of states, where each 2 char state code points
+    // to a TreeMap of places, with the keys being the name of the place (compared using the PlacesCompare Comparator).
+    // This lets us do lookups in log m time, where m is the number of places in the given state.
+    private HashMap<String,TreeMap<String,Place>> lookup_table;
 }
